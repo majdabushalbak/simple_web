c16a387211f441155981b0f8f127c5b5b329f8e2
MohammadShawakha <mohammad.shawakha@students.alquds.edu>
2024-08-27 13:41:22 +0300

    card component + fix the seeder

---
+++ resources/views/components/products/card.blade.php
@@ -0,0 +1,25 @@
+<div class="card h-100" onclick="location.href='{{ route('products.show', $product) }}'">
+    <!-- Image with fixed size -->
+    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"
+        class="card-img-top">
+    <div class="card-body">
+        <h5 class="card-title">{{ $product->name }}</h5>
+        <p class="card-text"><strong>السعر:</strong> ${{ $product->price }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+
+                @if (Auth::check() && Auth::user()->role === 1)
+                    <a href="{{ route('products.edit', $product) }}"
+                        class="btn btn-sm btn-outline-dark">تعديل</a>
+                    <form action="{{ route('products.destroy', $product) }}" method="POST"
+                        class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-sm btn-outline-dark"
+                            onclick="return confirm('هل أنت متأكد أنك تريد حذف هذا المنتج؟')">حذف</button>
+                    </form>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -38,31 +38,7 @@
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 @foreach ($products as $product)
                     <div class="col">
-                        <div class="card h-100" onclick="location.href='{{ route('products.show', $product) }}'">
-                            <!-- Image with fixed size -->
-                            <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"
-                                class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $product->name }}</h5>
-                                <p class="card-text"><strong>السعر:</strong> ${{ $product->price }}</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-
-                                        @if (Auth::check() && Auth::user()->role === 1)
-                                            <a href="{{ route('products.edit', $product) }}"
-                                                class="btn btn-sm btn-outline-dark">تعديل</a>
-                                            <form action="{{ route('products.destroy', $product) }}" method="POST"
-                                                class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-sm btn-outline-dark"
-                                                    onclick="return confirm('هل أنت متأكد أنك تريد حذف هذا المنتج؟')">حذف</button>
-                                            </form>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                  <x-products.card :product="$product"/>
                     </div>
                 @endforeach
             </div>
